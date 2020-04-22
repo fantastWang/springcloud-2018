0787ac33efe0b88d269a395d346ebc6eff79905e
fantast <1061505447@qq.com>
2020-04-22 15:34:47 +0800

    服务发现和Eureka集群

--- microservicecloud-provider-dept-8001/src/main/java/com/it/wcj/MicroservicecloudProviderDept8001Application.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/it/wcj/MicroservicecloudProviderDept8001Application.java
@@ -2,10 +2,12 @@ package com.it.wcj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient     //在服务启动后自动注册进eureka服务中心
+@EnableDiscoveryClient  //服务发现
 public class MicroservicecloudProviderDept8001Application {
 
     public static void main(String[] args) {
--- microservicecloud-provider-dept-8001/src/main/java/com/it/wcj/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/it/wcj/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.it.wcj.controller;
 import com.it.wcj.entities.Dept;
 import com.it.wcj.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,10 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    /**实现服务发现**/
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
@@ -25,5 +31,18 @@ public class DeptController {
     @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
     public List<Dept> list(){
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery",method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        System.out.println("***"+list);
+        //对应配置文件中的服务名
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("microservicecloud-provider-dept-8001");
+        for (ServiceInstance instance:serviceInstances){
+            System.out.println(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()
+            +"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
